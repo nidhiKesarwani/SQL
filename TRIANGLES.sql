beb5b653d3f602912f51eab9127581bc3a8a344c
nidhiKesarwani <kesarnidhi10@gmail.com>
2020-03-24 04:53:42 +0530

    Create TRIANGLES.sql

---
+++ TRIANGLES.sql
@@ -0,0 +1,10 @@
+/*
+Enter your query here.
+*/
+-- https://www.hackerrank.com/challenges/what-type-of-triangle/problem
+SELECT CASE  WHEN (A+B<=C OR B+C<=A OR A+C<=B) THEN 'Not A Triangle'
+             WHEN (A=B AND B=C ) THEN 'Equilateral'
+             WHEN (A=B OR B=C OR C=A) THEN 'Isosceles'
+             ELSE 'Scalene'
+       END
+FROM TRIANGLES;
